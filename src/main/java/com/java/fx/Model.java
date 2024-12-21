7778de278bae93ec08b37494d80709b2cd621b9a
Jacob Martinez <jacobmdz999@gmail.com>
2024-12-21 12:25:07 -0600

    add class Model

---
+++ src/main/java/com/java/fx/Model.java
@@ -0,0 +1,23 @@
+package com.java.fx;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Model{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name, phone;
+
+    @Override
+    public String toString(){
+        return "ID: "+id+"   -   Name: "+name+"   -   Phone: "+phone;
+    }
+}
